a95548cb2fbc13a051766064950c8975fd804634
micky387 <mickaelsaibi@free.fr>
2023-10-18 17:50:53 +0200

    gta4lwifi: Switch to Wifi aidl
    Google have dropped the hidl support, it's an vendor HAL
    
    https://github.com/omnirom/android_hardware_interfaces/commit/7b65166e336e7832a80a32e96d4a205fa47011d8
    
    Change-Id: I13a2f2698f6119bc008cc7fda842dec6c559b2c8

--- device.mk
+++ device.mk
@@ -369,7 +369,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     WifiOverlay \
-    android.hardware.wifi@1.0-service.legacy \
+    android.hardware.wifi-service \
     hostapd \
     libwifi-hal-qcom \
     libwpa_client \
