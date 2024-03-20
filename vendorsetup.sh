1ced46d97c99e04ba77cb81a1d457c6202250933
CuriousNom <aryanbiswas7585@gmail.com>
2024-03-21 02:34:12 +0530

    gta4lwifi: Switch to KernelSU and update ksu targets
    Signed-off-by: CuriousNom <aryanbiswas7585@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,4 +7,14 @@ cp -r hardware/tmp/* hardware/samsung/
 rm -rf hardware/tmp
 
 # Cloning kernel repositories
-git clone --depth=1 https://github.com/Matrixx-Devices/android_kernel_samsung_gta4l -b 14.0 kernel/samsung/gta4l
+git clone --depth=1 https://github.com/Matrixx-Devices/android_kernel_samsung_gta4l -b 14.0-ksu kernel/samsung/gta4l
+
+# Update KSU Targets
+cd kernel/samsung/gta4l
+rm -rf KernelSU
+git submodule sync
+git submodule update --init --recursive
+cd KernelSU
+git checkout main
+git pull
+cd ../../../..
